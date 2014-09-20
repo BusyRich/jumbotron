22cd0e74041c25068c629b705c499a17c173d3d1
Richard Key <rich@busyrich.com>
2014-09-19 17:17:15 -0700

    Added a bundle command that helps with remote deployment and easy sharing

--- bin/jumbotron.js
+++ bin/jumbotron.js
@@ -1,19 +1,36 @@
 #!/usr/bin/env node
-var fs = require('fs'),
-    cli = require('nomnom'),
+var util = require(__dirname + '/../lib/util'),
+    cli = require('nomnom').script('jumbotron'),
     log = require(__dirname + '/../lib/log'),
-    wDir = process.cwd();
+    commands = {
+      bundle: require(__dirname + '/commands/bundle')
+    };
 
-args = cli.parse()._;
+cli
+  .option('portable', {
+      abbr: 'p',
+      flag: true
+   })
+   .option('out', {
+       abbr: 'o'
+    });
 
-if(args.length > 0) {
-  wDir = args[0];
-}
+var opts = cli.nom();
+    args = opts._;
 
-wDir = require('path').resolve(wDir);
+if(args.length === 0 || !commands[args[0]]) {
 
-if(!fs.existsSync(wDir)) {
-  log.error('"%s" is not a directory', wDir);
-} else {
-  require(__dirname + '/../server')(wDir).start();
+  // get the working/project directory for the Jumbotron server
+  var wDir = process.cwd();
+  if(args.length > 0) {
+    wDir = util.getDir(args[0]);
+  }
+
+  if(!wDir) {
+    log.error('Cannot Run Jumbotron: Directory does not exist "%s"', args[0]);
+  } else {
+    require(__dirname + '/../server')(wDir).start();
+  }
+} else if(args[0] === 'bundle') {
+  commands.bundle(opts, args);
 }
